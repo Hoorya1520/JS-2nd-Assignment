345c648c1db46403eb0f2c6410e0717e42654b03
Hoorya1520 <Pakistanzindabad1520>
2025-09-03 15:21:16 -0700

    Java Script Assignment no 2

---
+++ app.js
@@ -0,0 +1,22 @@
+// Question #1
+var username;
+// Question # 2
+var myName = "Hoorya Alamgir"
+// Question # 3
+var message;
+message = "Hello World"
+alert(message)
+// Question # 4
+var studentName = "Hoorya Alamgir"
+var studentAge = "16"
+var studentCourse = "Web Development"
+// Question # 5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// Question # 6
+var email = "example@example.com" 
+alert("My email adress is example@example.com")
+// Question # 7
+var book = "A smarter Way to learn JavaScript"
+alert("I am trying to learn javascript from book A smarter way to learn javascript")
+// Question # 8
+document.write("Yah! I can write HTML content through Java Script")
